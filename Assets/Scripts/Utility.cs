2e25df3dcd942df7217b7f78d2f6ab32e8ae3f9e
hikariakio <31139816+hikariakio@users.noreply.github.com>
2020-01-31 20:06:02 +0630

    Uti

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    public static Vector3 NearestVector(Vector3 vect)
+    {
+        return new Vector3(Mathf.RoundToInt(vect.x), Mathf.RoundToInt(vect.y), Mathf.RoundToInt(vect.z));
+    }
+}
