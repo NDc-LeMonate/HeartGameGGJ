37ee2c831915c17a03a0e1ba38fbbdc126674683
tra371 <60510139+tra371@users.noreply.github.com>
2020-02-01 16:56:00 +0630

    MainMenu

---
+++ Assets/MainMenu/script/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+  
+    public void PlayGame() //to be attached to main menu
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
